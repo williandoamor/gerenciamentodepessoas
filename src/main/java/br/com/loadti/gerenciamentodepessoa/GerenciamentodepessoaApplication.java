792278d3f1e85762c159a90528eeb3297bd841a3
williandoamor <willsazon@gmail.com>
2020-11-20 17:24:51 -0300

    Organizando o projeto.

---
+++ src/main/java/br/com/loadti/gerenciamentodepessoa/GerenciamentodepessoaApplication.java
@@ -0,0 +1,13 @@
+package br.com.loadti.gerenciamentodepessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentodepessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentodepessoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/loadti/gerenciamentodepessoa/GerenciamentodepessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.loadti.gerenciamentodepessoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentodepessoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
