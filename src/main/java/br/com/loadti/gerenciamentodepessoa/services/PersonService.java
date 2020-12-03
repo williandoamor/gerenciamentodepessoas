2ba2c48fbaf264d8fd8bd1b9f31423ac2588cf4d
williandoamor <willsazon@gmail.com>
2020-12-03 20:14:17 -0300

    Criacao do metodo listAll.

--- src/main/java/br/com/loadti/gerenciamentodepessoa/services/PersonService.java
+++ src/main/java/br/com/loadti/gerenciamentodepessoa/services/PersonService.java
@@ -8,6 +8,9 @@ import br.com.loadti.gerenciamentodepessoa.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PersonService {
 
@@ -28,5 +31,14 @@ public class PersonService {
                 .builder()
                 .message("Pessoa criada com o id " + savedPerson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+
+        List<Person> allPeople = repository.findAll();
+        return allPeople.stream().map(
+                personMapper::toDTO
+        ).collect(Collectors.toList());
+
     }
 }
